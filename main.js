3892d4584565fe3e7b7a227d90a18f757252ecdd
LakhindarPal <72681474+LakhindarPal@users.noreply.github.com>
2024-02-05 13:09:49 +0530

    cache data

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const apiUrl = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/";
 
 // Get current date in DD/MM format
-const dateData = (param) => {
+const getDateData = (param) => {
   const date = param ? new Date(param) : new Date();
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
@@ -31,27 +31,63 @@ function openTab(tabName) {
 }
 
 // fetch data and add to sections 
-async function fetchAndLoadDataOnce(date) {
+async function fetchData(dateParam) {
+  const cached = JSON.parse(localStorage.getItem("onthisday"));
+  let data = cached?.data;
+  const date = getDateData(dateParam).apiDate;
 
-  const res = await fetch(`${apiUrl}${dateData(date).apiDate}`);
-  const data = await res?.json();
+  if (!cached || cached.date !== date || dateParam) {
+    const res = await fetch(`${apiUrl}${date}`);
+    if (!res.ok) return loadDataOnce("_", false);
+    const json = await res?.json();
 
+    data = Object.fromEntries(
+      Object.entries(json).map(([section, items]) => [
+      section,
+      items.map(({ text, year, pages }) => ({
+          text,
+          year,
+          pages: pages.map(({ titles, originalimage, description, extract_html, content_urls }) => ({
+            title: titles.normalized,
+            image: {
+              source: originalimage?.source || "",
+              width: originalimage?.width || "",
+              height: originalimage?.height || ""
+            },
+            description: description || "",
+            html: extract_html || "",
+            url: content_urls.desktop.page || ""
+          }))
+        }))
+      ])
+    );
+
+    if (!dateParam) {
+      localStorage.setItem("onthisday", JSON.stringify({ date, data }));
+    }
+  }
+  return data;
+}
+
+async function loadDataOnce(dateParam) {
+  const data = await fetchData(dateParam);
   const sectionIds = ["selected", "births", "deaths", "holidays", "events"];
 
   sectionIds.forEach(secId => {
     const content = data[secId] || [];
     const containerEl = document.querySelector(`#${secId} div.container`);
 
-    if (!res.ok) {
-      containerEl.innerHTML = `<p class="errorText">No data found for the requested date.</p>`;
+    if (!data) {
+      containerEl.innerHTML =
+        `<p class="errorText">No data found for the requested date.</p>`;
     } else {
       containerEl.innerHTML = "";
       content.forEach(item => {
         const articleEl = document.createElement("article");
         articleEl.classList.add("incident");
-        articleEl.innerHTML = `${item.year ? `<h3>Year ${item.year}</h3>` : ""}
-      <p>${item.text}</p>`;
-
+        articleEl.innerHTML =
+          `${item.year ? `<h3>Year ${item.year}</h3>` : ""}
+          <p>${item.text}</p>`;
         articleEl.addEventListener("click", () => showDetails(item));
 
         containerEl.appendChild(articleEl);
@@ -60,23 +96,23 @@ async function fetchAndLoadDataOnce(date) {
   });
 
   const h1Span = document.querySelector("h1 span");
-  h1Span.textContent = dateData(date).longDate;
+  h1Span.textContent = getDateData(dateParam).longDate;
 }
 
 // detailed information of a incident
 function showDetails(data) {
   const figureEl = (page) => (page.originalimage?.source) ?
     `<figure>
-      <img src="${page.originalimage.source}" alt="${page.description}" width="${page.originalimage.width}" height="${page.originalimage.height}">
+      <img src="${page.image.source}" alt="${page.description}" width="${page.image.width}" height="${page.image.height}">
       ${page.description?`<figcaption>${page.description}</figcaption>`:""}
     </figure>` : "";
 
   modalContent.innerHTML = data.pages.map(page =>
     `<article class="page">
-      <h3>${page.titles.display}</h3>
+      <h3>${page.title}</h3>
       ${figureEl(page)}
-      ${page.extract_html}
-      <a href="${page.content_urls.desktop.page}" target="_blank">Read more</a>
+      ${page.html}
+      <a href="${page.url}" target="_blank">Read more</a>
     </article>`
   ).join("");
 
@@ -96,7 +132,7 @@ closeBtn.addEventListener("click", () => {
 });
 
 async function initializeContentOnce(date) {
-  await fetchAndLoadDataOnce(date);
+  await loadDataOnce(date);
   openTab("selected");
 }
 
@@ -104,9 +140,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   await initializeContentOnce();
 });
 
-dateInput.addEventListener("change", () => {
+dateInput.addEventListener("change", async () => {
   const selectedDate = dateInput.value;
-  initializeContentOnce(selectedDate);
+  await initializeContentOnce(selectedDate);
 });
 
 // event listeners to tab buttons
